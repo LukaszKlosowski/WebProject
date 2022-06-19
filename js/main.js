b60dd1e4565c16742d42693d3b5048f31d3c07d3
LukaszKlosowski <57619175+LukaszKlosowski@users.noreply.github.com>
2022-06-19 21:46:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnHeader = document.querySelector('.header__btn');
+const headerTextBox = document.querySelector('.header__text-box')
+const navDesktop = document.querySelector('.nav-desktop');
+
+const viewportWidth = window.innerWidth;
+
+headerTextBox.classList.toggle('text-slick', viewportWidth < 992)
+
+btnHeader.addEventListener('click', () => {
+	location.href = '#services';
+});
